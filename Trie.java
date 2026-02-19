fb4aa433f317accecf39ca11631a90438f54481e
aarushpawar <aarush711pawar@gmail.com>
2026-02-19 10:06:15 -0500

    Remove main method from Trie

--- Trie.java
+++ Trie.java
@@ -183,43 +183,4 @@ public class Trie {
             currentWord.deleteCharAt(currentWord.length() - 1);
         }
     }
-
-	public static void main(String[] args) {
-		Trie trie = new Trie();
-
-		// Space-separated word block (nice & readable)
-		String data = """
-			apple banana apple apple
-			and and and any any
-			cat dog dog any any
-			apple any banana any
-			""";
-
-		// Load words
-		for (String w : data.split("\\s+")){ // split on white space
-			trie.insert(w.toLowerCase());
-		}
-
-		System.out.println("---- contains ----");
-		System.out.println("contains(\"apple\") --> " + trie.contains("apple"));
-		System.out.println("contains(\"banana\") --> " + trie.contains("banana"));
-		System.out.println("contains(\"ban\") --> " + trie.contains("ban"));
-		System.out.println("contains(\"zebra\") --> " + trie.contains("zebra"));
-
-		System.out.println("\n---- mostLikelyNextChar ----");
-		System.out.println("mostLikelyNextChar(\"a\") --> " + trie.mostLikelyNextChar("a"));
-		System.out.println("mostLikelyNextChar(\"ap\") --> " + trie.mostLikelyNextChar("ap"));
-		System.out.println("mostLikelyNextChar(\"do\") --> " + trie.mostLikelyNextChar("do"));
-		System.out.println("mostLikelyNextChar(\"x\") --> " + trie.mostLikelyNextChar("x"));
-
-		System.out.println("\n---- mostLikelyNextWord ----");
-		System.out.println("mostLikelyNextWord(\"a\") --> " + trie.mostLikelyNextWord("a"));
-		System.out.println("mostLikelyNextWord(\"ap\") --> " + trie.mostLikelyNextWord("ap"));
-		System.out.println("mostLikelyNextWord(\"b\") --> " + trie.mostLikelyNextWord("b"));
-		System.out.println("mostLikelyNextWord(\"z\") --> " + trie.mostLikelyNextWord("z"));
-
-		System.out.println("\n---- printWordFrequencies ----");
-		trie.printWordFrequencies();
-		System.out.println("\nNOTE:  Alphabetical sorting also acceptable\n\n" );
-	}
 }
